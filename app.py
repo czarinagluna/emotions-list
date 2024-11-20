6e3d4f78769a4daf7827c69d94b58334e0815db9
Czarina Luna <czarinagarcialuna@gmail.com>
2024-11-20 16:09:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title='Name Your Emotion',page_icon='💭')
 
 st.markdown("""
     <style>
-        @import url('https://fonts.cdnfonts.com/css/andrea-canolli-01');
+        @import url('https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Lexend:wght@100..900&family=Red+Hat+Text:ital,wght@0,300..700;1,300..700&display=swap');
     </style>
 """, unsafe_allow_html=True)
 
